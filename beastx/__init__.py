17825a02d3161e22c6cb74b0a2f34806dacaf810
MRUNAL <89269031+Mrunal1911@users.noreply.github.com>
2021-12-19 17:44:29 +0530

    Update __init__.py

--- beastx/__init__.py
+++ beastx/__init__.py
@@ -22,9 +22,7 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 
 from var import Var
-token = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
-
-sedmrunal = TelegramClient('sedmrunal', Var.APP_ID, Var.API_HASH).start(bot_token=token) 
+ 
 
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
