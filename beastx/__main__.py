c16d9ab96e8fb6ea4ae5a4e5c2e3184372b84e74
MRUNAL <89269031+Mrunal1911@users.noreply.github.com>
2021-12-23 17:41:07 +0530

    Update __main__.py

--- beastx/__main__.py
+++ beastx/__main__.py
@@ -125,7 +125,7 @@ import glob
 async def a():
             test1 = await bot.get_messages(plugin_channel, None , filter=InputMessagesFilterDocument) ; total = int(test1.total) ; total_doxx = range(0, total)
             for ixo in total_doxx:
-                        mxo = test1[ixo].id ; await bot.download_media(await bot.get_messages(client, ids=mxo), "beastx/modules/")
+                        mxo = test1[ixo].id ; await bot.download_media(await bot.get_messages(bot, ids=mxo), "beastx/modules/")
                         ar = glob.glob("beastx/modules/*.py")
                         f = len(ar)
                         sed.info(f" loading {f} modules it may take 1 minute please wait")
